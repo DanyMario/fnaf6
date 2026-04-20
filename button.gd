675ec855d51931626421e923bbcb39ec0cf1e21c
daniel-munoz13 <daniel.munoz13@upr.edu>
2026-04-20 01:41:19 -0400

    organized the file system

--- button.gd
+++
@@ -1,5 +0,0 @@
-#class_name CameraTask extends Resource
-#
-#@export var title: String
-#@export var button_reference: TextureButton # Drag your UI button here
-#@export var camera_scene: PackedScene      # Drag your camera .tscn file here
